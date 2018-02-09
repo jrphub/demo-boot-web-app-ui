dd2647f70ad8a5798480fba2ceb2e4a02f508ae5
jrphub <jyotiranjanpattnaik@gmail.com>
2018-02-09 21:18:55 +0530

    database changed to mysql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-/* Run for in-memory database only */
+/* Uncomment for in-memory database only i.e. LOCAL*/
+/*
 insert into tbl_doc (id, description, link, title, user_id) values(01, 'yellow rose png', 'https://www.gstatic.com/webp/gallery3/1.png', 'Yellow Rose', 01);
 insert into tbl_doc (id, description, link, title, user_id) values(02, 'baby tux for user page', 'https://www.gstatic.com/webp/gallery3/2.png', 'Baby Tux', 01);
 insert into tbl_doc (id, description, link, title, user_id) values(03, 'Ludo PNG transparency', 'https://www.gstatic.com/webp/gallery3/3.png', 'Ludo', 01);
@@ -7,4 +8,4 @@ insert into tbl_doc (id, description, link, title, user_id) values(05, 'river bo
 insert into tbl_doc (id, description, link, title, user_id) values(06, 'Stone man walk', 'http://www.gstatic.com/webp/gallery/3.jpg', 'Stone man', 02);
 insert into tbl_doc (id, description, link, title, user_id) values(07, 'Tree Sky', 'http://www.gstatic.com/webp/gallery/4.jpg', 'Tree', 02);
 insert into tbl_doc (id, description, link, title, user_id) values(08, 'Playing with fire Street Play', 'http://www.gstatic.com/webp/gallery/5.jpg', 'Fire', 02);
-
+*/
